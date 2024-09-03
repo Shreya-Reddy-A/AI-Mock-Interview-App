d30cd2cd92893291ddd328f12e07ebb43c1177f5
Shreya Reddy A <122392746+Shreya-Reddy-A@users.noreply.github.com>
2024-09-03 15:36:56 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://ai-interview_owner:zTYnXJLP3Fb4@ep-icy-credit-a5p0z3u9.us-east-2.aws.neon.tech/ai-interview?sslmode=require',
+      url: '',
     }
   };
